874cb89c44d3da396790eb59093a451f300540b1
itriscan <64763160+itriscan@users.noreply.github.com>
2021-06-10 18:28:59 -0400

    avance 2
    se agregaron dos colecciones y en estas se agregaron datos

--- create_db.py
+++ create_db.py
@@ -4,3 +4,26 @@ from pymongo import MongoClient
 dbUrl = MongoClient("mongodb://localhost")
 
 db= dbUrl['FuSA']
+
+collection_archive = db['Archivos']
+collections_type_of_font = db['tipos_de_fuentes']
+
+new_archive = collection_archive.insert_one({
+    "ID" : 1,
+    "fecha_de_grabacion" : "mar 08 jun 2021",
+    "ciudad" : "Coyhaique",
+    "duracion" : "0:11",
+    "formato" : ".wav",
+    "Usuario" : {"RUT": 123456789,"nombre": "Nombre1", "apellido": "Apellido1"},
+    "segmentos" : [{
+        "ID":1,
+        "formato": ".wav",
+        
+    }]
+})
+
+new_font = collections_type_of_font.insert_one({
+    "tipo" : "lluvia",
+    "archivo" : [1,1,1]
+
+})
